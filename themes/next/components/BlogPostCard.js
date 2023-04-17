1acbb301884f6923efbbe4645f8c6b4e6c25f655
kitions <nsplknsplk@gmail.com>
2023-04-17 15:22:00 +0800

    feat: link's config

--- themes/next/components/BlogPostCard.js
+++ themes/next/components/BlogPostCard.js
@@ -29,19 +29,18 @@ const BlogPostCard = ({ post, index, showSummary }) => {
     <Card className='w-full'>
       <div
         key={post.id}
-        className='flex flex-col-reverse justify-between duration-300'>
-        <div className='lg:p-8 p-4 flex flex-col w-full'>
-          {/* 文章标题 */}
-          <SmartLink
-            {...aosProps}
-            href={post?.href}
+        className="flex flex-col-reverse justify-between duration-300"
+      >
+        <div className=" p-4 flex flex-col w-full">
+          <Link
+            href={`${BLOG.SUB_PATH}/${post.slug}`}
             passHref
-            className={`cursor-pointer text-3xl ${showPreview ? 'text-center' : ''} leading-tight text-gray-700 dark:text-gray-100 hover:text-blue-500 dark:hover:text-blue-400`}>
-            {siteConfig('POST_TITLE_ICON') && (
-              <NotionIcon icon={post.pageIcon} />
-            )}{' '}
-            <span className='menu-link'>{post.title}</span>
-          </SmartLink>
+            className={`cursor-pointer hover:underline text-2xl ${showPreview ? 'text-center' : ''
+              } leading-tight text-gray-700 dark:text-gray-100 hover:text-blue-500 dark:hover:text-blue-400`}>
+
+            <NotionIcon icon={post.pageIcon} /> {post.title}
+
+          </Link>
 
           <div
             {...aosProps}
@@ -80,9 +79,7 @@ const BlogPostCard = ({ post, index, showSummary }) => {
           </div>
 
           {(!showPreview || showSummary) && !post.results && (
-            <p
-              {...aosProps}
-              className='mt-4 mb-12 text-gray-700 dark:text-gray-300 text-sm font-light leading-7'>
+            <p className="mt-4 mb-24 text-gray-700 dark:text-gray-300 text-sm font-light leading-7" style={{ margin: 0, marginTop: 8 }}>
               {post.summary}
             </p>
           )}
@@ -102,14 +99,16 @@ const BlogPostCard = ({ post, index, showSummary }) => {
             </div>
           )}
 
-          <div className='text-right border-t pt-8 border-dashed'>
-            <SmartLink
-              href={post?.href}
-              className='hover:bg-opacity-100 hover:underline transform duration-300 p-3 text-white bg-gray-800 cursor-pointer'>
+          {/* <div className="text-right border-t pt-8 border-dashed">
+            <Link
+              href={`${BLOG.SUB_PATH}/${post.slug}`}
+              className="hover:bg-opacity-100 hover:underline transform duration-300 p-3 text-white bg-gray-800 cursor-pointer">
+
               {locale.COMMON.ARTICLE_DETAIL}
-              <i className='ml-1 fas fa-angle-right' />
-            </SmartLink>
-          </div>
+              <i className="ml-1 fas fa-angle-right" />
+
+            </Link>
+          </div> */}
         </div>
 
         {siteConfig('NEXT_POST_LIST_COVER', null, CONFIG) &&
